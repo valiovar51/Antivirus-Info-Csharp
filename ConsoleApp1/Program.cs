b07eab1cc0fb4472995a992abac360a3da3dad2b
valiovar51 <104339371+valiovar51@users.noreply.github.com>
2022-04-25 07:32:26 +0300

    Update Program.cs
    Translate to Russian

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,24 +31,24 @@ public class Program
 
     public enum WSC_SECURITY_PROVIDER : int
     {
-        // Represents the aggregation of all firewalls for this computer.
+        // Представляет совокупность всех брандмауэров для этого компьютера.
         WSC_SECURITY_PROVIDER_FIREWALL = 0x1,
-        // Represents the Automatic updating settings for this computer.
+        // Представляет параметры автоматического обновления для этого компьютера.
         WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS = 0x2,
-        // Represents the aggregation of all antivirus products for this comptuer.
+        // Представляет совокупность всех антивирусных продуктов для данного компьютера.
         WSC_SECURITY_PROVIDER_ANTIVIRUS = 0x4,
-        // Represents the aggregation of all antispyware products for this comptuer.
+        // Представляет совокупность всех антишпионских продуктов для данного компьютера.
         WSC_SECURITY_PROVIDER_ANTISPYWARE = 0x8,
-        // Represents the settings that restrict the access of web sites in each of the internet zones.
+        // Представляет параметры, ограничивающие доступ к веб-сайтам в каждой из интернет-зон.
         WSC_SECURITY_PROVIDER_INTERNET_SETTINGS = 0x10,
-        // Represents the User Account Control settings on this machine.
+        // Представляет параметры контроля учетных записей пользователей на этом компьютере.
         WSC_SECURITY_PROVIDER_USER_ACCOUNT_CONTROL = 0x20,
-        // Represents the running state of the Security Center service on this machine.
+        // Представляет текущее состояние службы центра безопасности на этом компьютере.
         WSC_SECURITY_PROVIDER_SERVICE = 0x40,
 
         WSC_SECURITY_PROVIDER_NONE = 0,
 
-        // Aggregates all of the items that Security Center monitors.
+        // Объединяет все элементы, отслеживаемые центром безопасности.
         WSC_SECURITY_PROVIDER_ALL = WSC_SECURITY_PROVIDER_FIREWALL |
                                                                 WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS |
                                                                 WSC_SECURITY_PROVIDER_ANTIVIRUS |
